66bcae47a283843ada5d6a00b807eac65d1ad626
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2023-12-17 09:46:45 -0700

    Add page link to menu

---
+++ blog/Class/ProjectTag.cs
@@ -0,0 +1,13 @@
+﻿namespace blog.Class
+{
+    public class ProjectTag
+    {
+        public string? Pname { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string? Overview { get; set; }
+        public string? GitHubLink { get; set; }
+        public string? photoLink { get; set; }
+        public Dictionary<string, string>? TagLink { get; set; }
+    }
+}
