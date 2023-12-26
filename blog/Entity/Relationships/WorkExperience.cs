e4406d086f1fa54accda06dd3ad4dcb0313bd41a
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2023-12-26 13:52:16 -0700

    Create timeline for exerience, professional

---
+++ blog/Entity/Relationships/WorkExperience.cs
@@ -0,0 +1,21 @@
+﻿namespace blog.Entity
+{
+    public class WorkExperience
+    {
+        public string? Title { get; set; }
+
+        public string? Company {  get; set; }
+        
+        public string? City { get; set; }
+
+        public string? State { get; set; }
+
+        public string? Country { get; set; }
+
+        public DateTime? startDate { get; set; }
+
+        public DateTime? endDate { get; set; }
+
+        public List<string> Responsibilities { get; set; } = new List<string>();
+    }
+}
