76cc82283d304a31bd2708192cbac21318933850
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2023-12-28 14:57:30 -0700

    add skills section

---
+++ blog/Entity/Types/Skill.cs
@@ -0,0 +1,9 @@
+﻿namespace blog.Entity
+{
+    public class Skill
+    {
+        public string Name { get; set; } = "";
+        public int Level { get; set; } = 100;
+        public string Image { get; set; } = "";
+    }
+}
