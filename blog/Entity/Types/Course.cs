dc1bd6da17e2d1d760f495e0fecd718a3caf3e59
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2024-01-01 16:16:15 -0700

    Add courses and new projects

---
+++ blog/Entity/Types/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace blog.Entity
+{
+    public class Course
+    {
+        public string Name { get; set; } = "";
+        public string Grade { get; set; } = "";
+        public string Description { get; set; } = "";
+    }
+}
+
